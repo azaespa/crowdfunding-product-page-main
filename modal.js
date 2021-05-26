5288fc8fcce14fa1b97eb4986d6f571cde42821e
letsdothispej <pauljamesazanes@gmail.com>
2021-05-27 01:57:30 +0800

    modal card styling script

--- modal.js
+++ modal.js
@@ -1,7 +1,21 @@
 const INTRODUCTION_SECTION = document.querySelector('.introduction-section'),
  BACK_THIS_PROJECT_BTN = INTRODUCTION_SECTION.querySelector('.back-this-project'),
  MODAL_PROJECT = document.querySelector('.modal.project'),
- CLOSE_MODAL_PROJECT = MODAL_PROJECT.querySelector('.close-modal');
+ CLOSE_MODAL_PROJECT = MODAL_PROJECT.querySelector('.close-modal'),
+ MODAL_CARD_SELECT_LIST = MODAL_PROJECT.querySelectorAll('.modal-content-card-select');
+
+function styleSelectedPledge(object){
+ object.closest('.modal-content-card-container').classList.add("selected-card");
+}
+
+function handleSelectPledge(nodeList) {
+ for(let i=0; i <nodeList.length; i++){
+  console.log(nodeList[i].checked === true)
+  if(nodeList[i].checked === true) {
+   
+  }
+ }
+}
 
 function topFunction(){
  document.body.scrollTop = 0; // For Safari
@@ -13,9 +27,10 @@ function handleCloseModal() {
 }
 
 function paintModal() {
+ topFunction();
  MODAL_PROJECT.classList.add("showing-modal");
  CLOSE_MODAL_PROJECT.addEventListener("click", handleCloseModal);
- topFunction();
+ handleSelectPledge(MODAL_CARD_SELECT_LIST);
 }
 
 function handleClick() {
